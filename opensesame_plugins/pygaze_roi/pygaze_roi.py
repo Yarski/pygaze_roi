b81b214cfe37df63c2ede9a80418b29d013e8a2e
Yavor Ivanov <yivanov94@gmail.com>
2018-04-24 15:13:57 +0200

    first commit - Yavor

--- opensesame_plugins/pygaze_roi/pygaze_roi.py
+++ opensesame_plugins/pygaze_roi/pygaze_roi.py
@@ -50,7 +50,6 @@ class pygaze_roi(item):
 			pass
 
 	def coroutine(self, coroutines=None):
-
 		try:
 			eyetracker = self.experiment.pygaze_eyetracker
 		except AttributeError:
@@ -66,29 +65,142 @@ class pygaze_roi(item):
 				u'%s does not have a canvas attribute'
 				% self.var.linked_sketchpad
 			)
-		yield # End preparation
+
+
+		objects = [name for name, element in canvas]
+
 		samplenr = 0
+		fixating = False
+		x_prev, y_prev = 0,0
+		aoi_prev = []
+		fixation_prev = 0
+		dwell = []
+		fix_latency = []
+		fix_dwell = 0
+		fix_dwell_prev = 0
+		fix_location = []
+		time_fix = 0
+		time_sacc = 0
+		sample_n = self.var.duration/self.var.sampling_rate
+
+		dwell_time_dict = {
+			name: 0
+			for name, elements in canvas
+			if name != '__background__'
+		}
+
+
+		yield # End preparation ----------------------------------------------
+
+		t_0 = self.clock.time()
+		t_1 = 0
+
 		while True:
 			# Only collect data every once in a while so that we don't poll the
-			# eyetracker infinitely fast.
+			# eyetracker infinitely fast:
 			if not self.clock.once_in_a_while(self.var.sampling_rate):
 				alive = yield
 				if not alive:
 					break
 				continue
-			# Get a sample and check in which elements it is.
-			x, y = eyetracker.sample()
+
+			# Get a sample and check in which elements it is:
+			x,y = eyetracker.sample()
 			if self.var.uniform_coordinates:
 				x -= xc
 				y -= yc
-			fixated = canvas.elements_at(x, y)
-			for name, element in canvas:
-				self.experiment.var.set(
-					u'fix_%s_%04d' % (name, samplenr),
-					1 if name in fixated else 0
-				)
+
+			# Check if gaze is stable with a VERY arbitrary constraint
+			# of 5 points (x = current sample, xo = previous sample):
+			if abs((x-x_prev)+(y-y_prev)) < 5:
+				fixating = True
+				time_fix += self.var.sampling_rate
+
+				# Check in which canvas elements the fixation is:
+				aoi = [
+					name
+					for name in canvas.elements_at(x, y)
+					if name != '__background__'
+				]
+
+				for name in aoi:
+					dwell_time_dict[name] += 1
+
+			else:
+				# If fixation is not stable, aoi = NA:
+				fixating = False
+				time_sacc += self.var.sampling_rate
+				aoi = []
+
+			# Check for the beginning of a fixation and calculate its onset
+			# Fixation latencies relative to t_0 are stored in a list (fix_latency)
+			# and so are fixation locations (fix_location):
+			if not fixation_prev and fixating and aoi:
+				t_1 = self.clock.time()
+				fix_latency.append(t_1-t_0)
+				fix_location.append(aoi[0])
+			# Or, if fixation persists from last sample, start counting number of samples
+			# spent fixating:
+			elif fixation_prev and fixating and aoi:
+				fix_dwell += 1
+			else:
+				pass
+
+			# Append the accumulated fix_dwell samples once a saccade is initiated
+			# after a period of aoi fixation:
+			if not fixating and fixation_prev and fix_dwell and aoi_prev:
+				dwell.append(fix_dwell)
+
+
+			# Save the current sample's values for use in next iteration:
+			x_prev, y_prev = x,y
+			aoi_prev = aoi
+			fixation_prev = fixating
+			fix_dwell_prev = fix_dwell
 			samplenr += 1
 
+		for i, value in dwell_time_dict.items():
+			self.experiment.var.set(
+				u'dwell_time_%s' % i,
+				value*self.var.sampling_rate
+				)
+
+
+		if len(fix_latency) != 0:
+			self.experiment.var.set(
+				u'first_fix_latency',
+				fix_latency[0]
+				)
+		else:
+			self.experiment.var.set(
+				u'first_fix_latency',
+				0
+				)
+
+
+		if len(fix_location) != 0:
+			#fix_location = list(filter(None, fix_location))
+			self.experiment.var.set(
+				u'first_fix_location',
+				str(fix_location[0])
+				)
+		else:
+			self.experiment.var.set(
+				u'first_fix_location',
+				0
+				)
+
+
+		if len(dwell) != 0:
+			self.experiment.var.set(
+				u'first_fix_dwell',
+				dwell[0]*self.var.sampling_rate
+				)
+		else:
+			self.experiment.var.set(
+				u'first_fix_dwell',
+				0
+				)
 
 class qtpygaze_roi(pygaze_roi, qtautoplugin):
 
